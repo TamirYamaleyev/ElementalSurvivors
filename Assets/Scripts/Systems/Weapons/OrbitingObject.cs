7e695d95397ae7d0536ab38b14a34a91669836ff
Tamir Yamaleyev <tamirka8@gmail.com>
2026-06-13 16:14:23 +0300

    Created Shuriken weapon

--- Assets/Scripts/Systems/Weapons/OrbitingObject.cs
+++ Assets/Scripts/Systems/Weapons/OrbitingObject.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class OrbitingObject : MonoBehaviour
 {
+    [SerializeField] private float spinSpeed = 50f;
+
     private float angleOffset;
     private float radius;
     private float speed;
@@ -27,18 +29,35 @@ public class OrbitingObject : MonoBehaviour
 
     {
         angleOffset = (Mathf.PI * 2f / total) * index;
+
         this.radius = radius;
         this.speed = speed;
+
         this.damage = damage;
         this.status = status;
         this.statusDuration = statusDuration;
         this.statusSystem = statusSystem;
+
         this.center = center;
     }
 
     void Update()
     {
-        float angle = Time.time * speed * angleOffset;
+        UpdatePosition();
+        Spin();
+    }
+
+    private void Spin()
+    {
+        transform.Rotate(0f, 0f, spinSpeed * Time.deltaTime);
+    }
+
+    private void UpdatePosition()
+    {
+        if (center == null)
+            return;
+
+        float angle = Time.time * -speed + angleOffset;
 
         Vector2 offset = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * radius;
         transform.position = (Vector2)center.position + offset;
