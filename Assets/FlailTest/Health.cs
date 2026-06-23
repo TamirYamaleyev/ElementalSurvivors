c46f77029e1e77e4be912ff3f0afa6fa32b387f4
Tamir Yamaleyev <tamirka8@gmail.com>
2026-06-23 15:40:21 +0300

    uhhhh

--- Assets/FlailTest/Health.cs
+++ Assets/FlailTest/Health.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField] private string GAME_SCENE_NAME;
+
     public float hp = 100;
 
     public void TakeDamage(float amount)
@@ -9,5 +12,10 @@ public class Health : MonoBehaviour
         hp -= amount;
 
         Debug.Log($"Took {amount} damage\nHP: {hp}");
+
+        if (hp <= 0 )
+        {
+            SceneManager.LoadScene( GAME_SCENE_NAME );
+        }
     }
 }
